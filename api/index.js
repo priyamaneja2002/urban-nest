b493d44b3446821efca3345e80f6643942ec4a9e
Priyam Aneja <priyamaneja2002@gmail.com>
2023-11-26 18:21:32 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,20 +1,19 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log("database connected!");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.use(express.json());
@@ -22,18 +21,16 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000!");
+  console.log('Server is running on port 3000!');
 });
-
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode, 
-        message
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
-import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
-import { verifyToken } from "../utils/verifyUser.js";
+import express from 'express';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 
 const router = express.Router();
 
